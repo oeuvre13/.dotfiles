21f9b1734afcd522d0a2c5f8ab64245843b9ab74
Andrew <57809500+oeuvre13@users.noreply.github.com>
2025-01-14 10:24:49 +0700

    refactor

--- setup.sh
+++ setup.sh
@@ -6,5 +6,6 @@ git clone --depth=1 https://github.com/mattmc3/antidote.git ${ZDOTDIR:-$HOME}/.a
 # install tpm (tmux)
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-# stow
-stow git nvim zsh tmux
+# symlink
+ln -s ~/.dotfiles/nvim ~/.config/nvim
+stow git zsh tmux
